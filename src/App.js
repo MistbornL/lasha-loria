d058b86fb1af3b298172c84cbb4aacc94b8d2b1e
Lasha <50033103+MistbornL@users.noreply.github.com>
2022-06-09 23:51:51 +0400

    mixin

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/PDP" element={<PDP />} />
-      <Route path="/card" element={<Cart />} />
+      <Route path="/cart" element={<Cart />} />
     </Routes>
   );
 }
