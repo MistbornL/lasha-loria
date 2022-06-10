71366bc0a1fea696607452fcb1324069f2534e2b
Lasha <50033103+MistbornL@users.noreply.github.com>
2022-06-10 15:08:42 +0400

    update

--- src/components/forHome/prodCard/ProdCard.jsx
+++ src/components/forHome/prodCard/ProdCard.jsx
@@ -1,12 +1,32 @@
 import "./card.scss";
 import shirt from "../../../assets/shirt.png";
 import React, { Component } from "react";
+import greenCart from "../../../assets/cartInGreen.png";
 
 export default class ProdCard extends Component {
+  constructor() {
+    super();
+    this.state = { isShown: false };
+  }
   render() {
     return (
-      <div className="prod-card-top">
+      <div
+        className="prod-card-top"
+        onMouseEnter={() => this.setState({ isShown: true })}
+        onMouseLeave={() => this.setState({ isShown: false })}
+      >
         <div className="prod-img">
+          <img
+            style={{
+              position: "absolute",
+              right: 0,
+              bottom: 0,
+              marginRight: "40px",
+              marginBottom: "-20px",
+            }}
+            src={this.state.isShown ? greenCart : null}
+            alt={this.state.isShown ? "greenCart" : null}
+          />
           <img src={shirt} alt="prod-img" />
         </div>
         <div className="desc">
