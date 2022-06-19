fb71bd69761cdab211cc132b55ccb639c34b69eb
Lasha <50033103+MistbornL@users.noreply.github.com>
2022-06-19 16:17:53 +0400

    category routes

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import PDP from "./pages/PDP/PDP";
 import Cart from "./pages/Cart/Cart";
 import { Home } from "./pages/Home/Home";
 import { Clothes } from "./pages/Home/categories/Clothes";
+import { Tech } from "./pages/Home/categories/Tech";
 
 function App() {
   return (
@@ -13,7 +14,7 @@ function App() {
       <Route path="/PDP/:id" element={<PDP />} />
       <Route path="/cart" element={<Cart />} />
       <Route path="/clothes" element={<Clothes />} />
-      {/* <Route path="/tech" element={<Cart />} /> */}
+      <Route path="/tech" element={<Tech />} />
     </Routes>
   );
 }
---
+++ src/pages/Home/categories/Tech.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { GET_TECH_CATEGORIES } from "../../../GraphQL/Queries";
+import { useDispatch, useSelector } from "react-redux";
+import { useQuery } from "@apollo/client";
+import Header from "../../../components/forHome/header/Header";
+import { storeProduct } from "../../../state/reducer";
+import ProdCard from "../../../components/forHome/prodCard/ProdCard";
+export const Tech = () => {
+  const dispatch = useDispatch();
+  const { loading, error, data } = useQuery(GET_TECH_CATEGORIES);
+  const selecteds = useSelector((state) => state.store);
+
+  if (loading) return "Loading...";
+  if (error) return `Error! ${error.message}`;
+  dispatch(storeProduct(data.category.products));
+
+  return (
+    <div className="App">
+      <Header />
+      <div className="category-name">
+        {selecteds.selectAll ? (
+          <h1>all</h1>
+        ) : selecteds.selectTech ? (
+          <h1>tech</h1>
+        ) : selecteds.selectCloth ? (
+          <h1>clothes</h1>
+        ) : (
+          <h1>all</h1>
+        )}
+      </div>
+
+      <main>
+        <section className="prod-card">
+          {data.category.products.map((item) => {
+            return (
+              <ProdCard
+                name={item.name}
+                price={item.prices[0].amount}
+                symbol={item.prices[0].currency.symbol}
+                img={item.gallery[0]}
+                id={item.id}
+                inStock={item.inStock}
+              />
+            );
+          })}
+        </section>
+      </main>
+    </div>
+  );
+};
