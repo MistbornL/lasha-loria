744f4b0c3cf1fc7d88947ac16097aebf1c13525f
Lasha <50033103+MistbornL@users.noreply.github.com>
2022-06-26 21:37:15 +0400

    cartpop

--- src/components/forHome/header/cartPop/CartPop.jsx
+++ src/components/forHome/header/cartPop/CartPop.jsx
@@ -1,14 +1,26 @@
 import React from "react";
-// import { useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import "./cartpop.scss";
 export const CartPop = () => {
-  //   const cartData = useSelector((state) => state.store.cart);
+  const cartData = useSelector((state) => state.store.cart);
+  console.log(cartData);
   return (
     <div className="cart-pop-wrapper">
-      <h1>My Bag</h1>
+      <h1>
+        My Bag, <span>{cartData.length} items</span>
+      </h1>
 
-      <section></section>
+      {cartData.map((item) => {
+        return (
+          <section>
+            <div className="card-pop-top">
+              <h1>{item.name}</h1>
+            </div>
+          </section>
+        );
+      })}
+
       <div className="cart-bottom">
         <Link to={"/cart"}>
           <button>VIEW BAG</button>
